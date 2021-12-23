eaaa241ba8574ce6938acb6cd8c8e884252eee27
SpookyThief <more.stupid.spam@gmail.com>
2021-12-23 13:40:29 +0100

    still don't work

--- commands/play.js
+++ commands/play.js
@@ -5,12 +5,17 @@ const {sounds} = require('../getSounds.js');
 module.exports = {
    data: new SlashCommandBuilder().setName('play')
       .setDescription('Plays specified sound')
-      .addStringOption(option => option.setName('sound').
-            setDescription('Specifies sound to play')),
+      .addStringOption(option => option.setName('sound')
+         .setDescription('Specifies sound to play')),
    async execute(interaction){
       const sound = interaction.option.getString('sound');
-      interaction.reply(`Sounds: ${sounds}`);
-      if (!value) return;
+      interaction.reply('moin');
+
+      if(!sound) return;
+      else interaction.reply(sound);
+      //const sound = interaction.option.getString('sound');
+      //interaction.reply(`Sounds: ${sounds}`);
+      //if (!value) return;
 
    },
 };
